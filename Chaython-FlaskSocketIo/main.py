d6e61bfc72f143f9a398f78677cf31ba83a9096e
RubenJoyfe <80394500+RubenJoyfe@users.noreply.github.com>
2022-02-21 12:05:03 +0100

    Login-Logout

--- Chaython-FlaskSocketIo/main.py
+++ Chaython-FlaskSocketIo/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify, abort, render_template, url_for, session, request
-from flask_socketio import SocketIO, send, join_room, leave_room, emit
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, abort
+from flask_socketio import SocketIO, join_room, leave_room, emit
 from flask_session import Session
 from distutils.dep_util import newer
 from datetime import datetime, timedelta
@@ -10,6 +10,7 @@ import requests, json, os, jwt
 import db
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'ContraseñaSuperSecretaQuenodeberíaSaberNADIE:=)'
 app.config['SESSION_TYPE'] = 'filesystem'
 # socketio = SocketIO(app, cors_allowed_origins="http://localhost")
@@ -17,8 +18,12 @@ JSON_KEY = 'ContraseñaSuperSecretaQuenodeberíaSaberNADIE:=)'
 
 Session(app)
 
+# socketio = SocketIO(app, manage_session=False)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if(session.get('name') is not None):
+        return render_template('chat.html', session = session) # return redirect(url_for('chat'))
     return render_template('index.html')
 
 @app.route('/Chaython', methods=['GET', 'POST'])
@@ -30,14 +35,18 @@ def chat():
         session['name'] = uname
         session['token'] = token
         # jsonify({"name": uname,"token": token}), 200
-        return render_template('chat.html', session=session)
+        return render_template('chat.html', session = session)
     else:
-        # if (session.get('name') is not None):
-        #     return render_template('chat.html', session=session)
-        # else:
-            # return render_template(url_for('index'))
-        return render_template('index.html')
+        if (session.get('name') is not None):
+            return render_template('chat.html', session = session)
+        else:
+            return redirect(url_for('index'))
+        # return render_template('index.html')
 
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 # @socketio.on('message')
 # def handleMessage(msg):
