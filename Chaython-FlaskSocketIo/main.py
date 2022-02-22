77c5bb70c3a3d5036fb2ba99584700baef1c9c02
RubenJoyfe <ruben.zunniga.carrillo@alumnojoyfe.iepgroup.es>
2022-02-22 19:06:01 +0100

    update

--- Chaython-FlaskSocketIo/main.py
+++ Chaython-FlaskSocketIo/main.py
@@ -21,35 +21,60 @@ Session(app)
 # socketio = SocketIO(app, cors_allowed_origins="http://localhost")
 # socketio = SocketIO(app, manage_session=False)
 
+def check_auth(redirect_name:str):
+    def decorator(f:str):
+        @wraps(f)
+        def wrapper(*args, **kargs):
+            try:
+                if(session.get('name') is None):
+                    return redirect(url_for('index'))
+                elif (redirect_name != ''):
+                    return redirect(url_for(redirect_name))
+            except Exception as e:
+                abort(401)
+            pass
+        return wrapper
+    return decorator
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if(session.get('name') is not None):
-        return render_template('chat.html', session = session) # return redirect(url_for('chat'))
+        return redirect(url_for('home'))
     return render_template('index.html')
 
-@app.route('/Chaython', methods=['GET', 'POST'])
-def chat():
-    if (request.method=='POST'):
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if(session.get('name') is not None):
+        return render_template('home.html', session = session) # return redirect(url_for('chat'))
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if(session.get('name') is not None):
+        return redirect(url_for('home'))
+    elif (request.method=='POST' and request.form['name'] is not None and request.form['password'] is not None):
         uname = request.form['name']
         pword = request.form['password']
         u = db.logIn(uname, pword)
         if u is None:
             return redirect(url_for('index')+'?error=1')
             # print(db.registerUser(uname, pword))
-        user_chat = [{"name": "luk chat"}, {"name": "carl chat"}]
+        user_chats = [{"name": "luk chat"}, {"name": "carl chat"}]
         token = jwt.encode({"id": u['_id'],"name": u['name']}, JSON_KEY, algorithm='HS256')
         session['name'] = uname
-        session['room'] = "default"
-        session['chats'] = user_chat
+        session['chats'] = user_chats
         session['token'] = token
         # jsonify({"name": uname,"token": token}), 200
+        return render_template('home.html', session = session)
+    return redirect(url_for('index'))
+
+@app.route('/Chaython', methods=['GET', 'POST'])
+def chat():
+    if (session.get('name') is not None and session['room'] is not None):
+        session['room'] = request.form['room']
         return render_template('chat.html', session = session)
     else:
-        if (session.get('name') is not None):
-            return render_template('chat.html', session = session)
-        else:
-            return redirect(url_for('index'))
-        # return render_template('index.html')
+        return redirect(url_for('index'))
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
